cffb959662a8491b399b26d078ad22f13e9843c2
DmitriySh <dashishmakov@gmail.com>
2017-11-08 07:09:42 +0300

    Add mongodb_exporter

--- mongodb-exporter/docker_build.sh
+++ mongodb-exporter/docker_build.sh
@@ -3,4 +3,4 @@
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
 
-docker build -f ./Dockerfile_1_0 -t $USERNAME/ui .
+docker build -t $USERNAME/mongodb_exporter ./
